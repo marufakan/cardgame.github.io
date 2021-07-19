ed8d3935ec3918298585166ee96e91b839f5eb15
marufakan01 <marufakan01@gmail.com>
2021-07-19 22:21:07 +0300

    Updated cardgame.github.io

--- cardScript.js
+++ cardScript.js
@@ -1,61 +1,80 @@
 $(document).ready(function(){
-  var clc=0;
-  var arr1=[];
-  var theColorIs1="";
-  
-  $(".flip-card").click(function(){
-    theColorIs1 = $($(this).children()).css("background-color");
-    //dont touch card
-    $(this).css("pointer-events","none");
+  $(".play-button").click(function(){
+    $(this).hide();
+    var second = 0, minute = 0;
+     //start  stop-watch
+    $("#stopwatch1").html(" 0: 0");
+    setInterval(stopWatch, 1000);
+    var clc=0;
+    var arr1=[];
+    var theColorIs1="";
+    var cardCounter=0;
 
-    arr1.push(theColorIs1);
-    if(arr1.length==2){
-      for(var i=0;i<arr1.length;i++){
-        for(var j=i+1;j<arr1.length;j++){
-          if(arr1[i]==arr1[j]){
-            $(".flip-card").css("pointer-events","auto");
-            $('div').filter(function() {
-              return $(this).css('background-color') == arr1[j];
-            }).each(function() {
-              $(this).parents().removeClass("flip-card");
+    $(".flip-card").click(function(){
+      //dont touch card
+      $(this).css("pointer-events","none");
+      theColorIs1 = $($(this).children()).css("background-color");
+      arr1.push(theColorIs1);
+      if(arr1.length==2){
+        for(var i=0;i<arr1.length;i++){
+          for(var j=i+1;j<arr1.length;j++){
+            if(arr1[i]==arr1[j]){
+              $(".flip-card").css("pointer-events","auto");
+              $('div').filter(function() {
+                return $(this).css('background-color') == arr1[j];
+              }).each(function() {
+                $(this).parents().removeClass("flip-card");
+                cardCounter++;
+                arr1= [];
+                theColorIs1="";
+              })
+            }
+            else{
+              $(".flip-card").css("pointer-events","auto");
               arr1= [];
-              theColorIs1="";
-            })
-          }
-          else{
-            $(".flip-card").css("pointer-events","auto");
-            arr1= [];
-
+            }
           }
         }
       }
-    }
-    if($(this).children().hasClass("flip-card-inner")){
-      console.log("d1 "+clc);
+      if($(this).children().hasClass("flip-card-inner")){
         $(this).children().removeClass("flip-card-inner");
-    }else{
-      theColorIs1 = $($(this).children()).css("background-color");
-      console.log("d2 "+clc);
-      clc++;
-      $(this).children().addClass("flip-card-inner");
-    }
-    if(clc%3==0 ){
-      console.log("d3 "+clc);
-      $(".flip-card").children().removeClass("flip-card-inner");
-      clc=0;
-      arr1= [];
-      theColorIs1="";
-    }
-    //add time
-    setTimeout(myTime, 2000);
-    function myTime(){
-      if(clc%2==0 ){
+      }else{
+        theColorIs1 = $($(this).children()).css("background-color");
+        clc++;
+        $(this).children().addClass("flip-card-inner");
+      }
+      if(clc%3==0 ){
         $(".flip-card").children().removeClass("flip-card-inner");
         clc=0;
         arr1= [];
         theColorIs1="";
       }
+      // added time for standby
+      setTimeout(myTime, 2300);
+      function myTime(){
+        if(clc%2==0 ){
+          $(".flip-card").children().removeClass("flip-card-inner");
+          $(".flip-card").css("pointer-events","auto");
+          clc=0;
+          arr1= [];
+          theColorIs1="";
+        }
+      }
+      if(cardCounter==16){
+        alert("Game rebooting!");
+        location.reload(true);
+      }
+    });
+    //stopwatch
+    function stopWatch(){
+      if(second < 59) second = second + 1;
+    else
+    {
+      second = 0; 
+      if(minute < 59) minute = minute + 1;
     }
-});
+    $("#stopwatch1").html( minute + " : " + second);
+   }
+  });
 });
 
--- myScript.js
+++ myScript.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+    //How can I block F12 keyboard key in jquery for all my pages and elements?
+    // Here 123 is the keyCode of F12 which opens the Inspect Element screen in the browser. Adding a keydown event than only does return false for 123 will block the Inspect Element screen.
+    $(document).keydown(function (event) {
+        if (event.keyCode == 123) { // Prevent F12
+            return false;
+        } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+            return false;
+        }
+    });
+    // Prevent Right Click > Inspect Element
+    $(document).on("contextmenu", function (e) {
+        e.preventDefault();
+    });
+
     var myColor=["red","brown","pink","orange","navy","green","purple","yellow","red","brown","pink","orange","navy","green","purple","yellow"];
     var repeatColor=[];
     var body=document.body;
@@ -6,8 +20,19 @@ $(document).ready(function(){
     div_container.className="container";
     var table=document.createElement("table");
     table.style = "width:100%";
-
     body.appendChild(div_container);
+    //play button
+    var play_button=document.createElement("button");
+    body.appendChild(play_button);
+    play_button.className="play-button";
+    play_button.innerHTML="START";
+    //stop-watch
+    var stop_watch=document.createElement("div");
+    body.appendChild(stop_watch);
+    $(stop_watch).attr('id', 'stopwatch1');
+    stop_watch.className="stop-watch";
+    stop_watch.innerHTML=" 0: 0";
+
     div_container.appendChild(table);
     for(let i = 0; i < 4; i++){
     var tr=document.createElement("tr")
@@ -21,7 +46,6 @@ $(document).ready(function(){
             td_card.appendChild(div_flip_card)
             var div_flip_card_inner1=document.createElement("div")
             div_flip_card_inner1.className = "flip-card-inner1"
-
             //random color add
             // Blocking more than 2 repetitions
             var rd;
@@ -44,11 +68,9 @@ $(document).ready(function(){
                 if(rp<2){
                     repeatColor.push(myColor[rd]);
                     $(div_flip_card_inner1).addClass(myColor[rd])
-                    console.log(myColor[rd]);
                     isRp=false;
                 }
             }
-
             div_flip_card.appendChild(div_flip_card_inner1)
             var div_flip_card_front = document.createElement("div")
             div_flip_card_front.className = "flip-card-front"
